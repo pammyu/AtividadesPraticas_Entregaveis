0af9292d54c4cc1d926303c0afab10a39efabb92
Pâmela dos Reis <pammreiss@gmail.com>
2025-11-04 00:21:47 -0300

    Acrescentei mais atividades resolvidas

--- atividadesEntregaveis/dia7_lacosRepeticao/exercicio7.js
+++ atividadesEntregaveis/dia7_lacosRepeticao/exercicio7.js
@@ -1,2 +1,17 @@
 //Exercício 7 - Vetores
 
+import readline from "readline-sync"
+
+let vetor = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6];
+let numeroEncontrar = readline.questionInt("Digite o numero que voce deseja encontrar: ");
+let posicaoFinal = null;
+
+
+for( let i = 0 ; i < vetor.length ; i++ ){
+    if(vetor[i] === numeroEncontrar){
+        posicaoFinal = i;
+    }
+}
+
+console.log((posicaoFinal === null) ? `O número ${numeroEncontrar} não foi encontrado!` :
+`O número ${numeroEncontrar} esta localizado na posição: ${posicaoFinal}`);
